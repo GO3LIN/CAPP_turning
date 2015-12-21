f3a983661b2b39232b0a6fd2bb0eecfe84008f2d
GO3LIN <goblinprog@gmail.com>
2015-12-21 18:01:13 -0500

    Added rule that compares max circles diameter with stock diameter

--- cylinderRecognition.py
+++ cylinderRecognition.py
@@ -30,82 +30,84 @@ class cylinderRecognition:
 		# 1th Rule :  Closed shell contains at least 2 advanced faces with the same cylindrical surface
 		if duplicated_cs:
 			# Find the cylindrical surface with the most diameter
-			'''max_diameter_cs = duplicated_cs[0]
+			#related_advanced_faces = closed_s.getAdvancedFacesFromSurfaceType(max_diameter_cs)
+			max_diameter_cs = duplicated_cs[0]
 			for ds in duplicated_cs:
 				if(max_diameter_cs.radius<ds.radius):
 					max_diameter_cs=ds
 			#print max_diameter_cs.radius
 			# 2nd Rule
 			if(stock.sd > (max_diameter_cs.radius*2)):
-			related_advanced_faces = closed_s.getAdvancedFacesFromSurfaceType(max_diameter_cs)'''
-			related_advanced_faces = []
-			for cs in duplicated_cs:
-				afs = closed_s.getAdvancedFacesFromSurfaceType(cs)
-				for af in afs:
-					related_advanced_faces.append(af)
-				#related_advanced_faces.append(closed_s.getAdvancedFacesFromSurfaceType(cs))
-			raf_temp = list(related_advanced_faces)
+			
+				related_advanced_faces = []
+				for cs in duplicated_cs:
+					afs = closed_s.getAdvancedFacesFromSurfaceType(cs)
+					for af in afs:
+						related_advanced_faces.append(af)
+					#related_advanced_faces.append(closed_s.getAdvancedFacesFromSurfaceType(cs))
+				raf_temp = list(related_advanced_faces)
 
-			while(len(raf_temp)>0):
-				first = raf_temp.pop()
-				first_lines = first.getEdgeType(stepCode.line)
-				first_lines_step = r_tools.getStepFromObjects(first_lines)
+				while(len(raf_temp)>0):
+					first = raf_temp.pop()
+					first_lines = first.getEdgeType(stepCode.line)
+					first_lines_step = r_tools.getStepFromObjects(first_lines)
+					for raf in related_advanced_faces:
+						if(first.stepLine != raf.stepLine):
+							raf_lines = raf.getEdgeType(stepCode.line)
+							raf_lines_step = r_tools.getStepFromObjects(raf_lines)
+							if(set(raf_lines_step) == set(first_lines_step)):
+								raf_temp.pop(raf_temp.index(raf))
+								self.foundFeatures.append([raf.stepLine, first.stepLine])
+
+							
+				'''
 				for raf in related_advanced_faces:
-					if(first.stepLine != raf.stepLine):
-						raf_lines = raf.getEdgeType(stepCode.line)
-						raf_lines_step = r_tools.getStepFromObjects(raf_lines)
-						if(set(raf_lines_step) == set(first_lines_step)):
-							raf_temp.pop(raf_temp.index(raf))
-							self.foundFeatures.append([raf.stepLine, first.stepLine])
-
-						
-			'''
-			for raf in related_advanced_faces:
-				print raf.stepLine
-			related_advanced_faces = closed_s.getAdvancedFacesFromStepSurfaceType(stepCode.cylindrical_surface)
+					print raf.stepLine
+				related_advanced_faces = closed_s.getAdvancedFacesFromStepSurfaceType(stepCode.cylindrical_surface)
 
 
-			for i,raf in enumerate(related_advanced_faces):
-				for j,raf2 in enumerate(related_advanced_faces):
-					if(i!=j):
-						raf_lines = raf.getEdgeType(stepCode.line)
-			'''
+				for i,raf in enumerate(related_advanced_faces):
+					for j,raf2 in enumerate(related_advanced_faces):
+						if(i!=j):
+							raf_lines = raf.getEdgeType(stepCode.line)
+				'''
 
 
-			'''
-			#print related_advanced_faces[0].getEdgeType(stepCode.line)[0].stepLine
-			af_lines = af_circles = []
-			for raf in related_advanced_faces:
-				raf_lines = raf.getEdgeType(stepCode.line)
-				raf_circles = raf.getEdgeType(stepCode.circle)
-				if len(raf_lines)==2 and len(raf_circles) == 2:
-					af_lines += raf_lines
-			# 3rd Rule
-			if(len(af_lines)%4==0):
-				af_lines_step = r_tools.getStepFromObjects(af_lines)
-				# 4th Rule
-				if(len(set(af_lines_step))==(len(af_lines)/2)):
-					circles = closed_s.getEdgeType(stepCode.circle)
-					circles_max_radius = circles[0].radius
-					for circle in circles:
-						if(circles_max_radius<circle.radius):
-							circles_max_radius = circle.radius
-					# 5th Rule 
-					if(circles_max_radius<=max_diameter_cs.radius):
-						print '---------- Cylinder feature found !'
-						wp = ap238.workpiece.__new__(ap238.workpiece)
-						operations = ap238.machining_operation.__new__(ap238.machining_operation)
+				'''
+				#print related_advanced_faces[0].getEdgeType(stepCode.line)[0].stepLine
+				af_lines = af_circles = []
+				for raf in related_advanced_faces:
+					raf_lines = raf.getEdgeType(stepCode.line)
+					raf_circles = raf.getEdgeType(stepCode.circle)
+					if len(raf_lines)==2 and len(raf_circles) == 2:
+						af_lines += raf_lines
+				# 3rd Rule
+				if(len(af_lines)%4==0):
+					af_lines_step = r_tools.getStepFromObjects(af_lines)
+					# 4th Rule
+					if(len(set(af_lines_step))==(len(af_lines)/2)):
+						circles = closed_s.getEdgeType(stepCode.circle)
+						circles_max_radius = circles[0].radius
+						for circle in circles:
+							if(circles_max_radius<circle.radius):
+								circles_max_radius = circle.radius
+						# 5th Rule 
+						if(circles_max_radius<=max_diameter_cs.radius):
+							print '---------- Cylinder feature found !'
+							wp = ap238.workpiece.__new__(ap238.workpiece)
+							operations = ap238.machining_operation.__new__(ap238.machining_operation)
 
-						#ap238.outer_diameter('OD1', wp, set())
-						self.max_diameter = max_diameter_cs.radius*2
-						self.advanced_faces = related_advanced_faces
+							#ap238.outer_diameter('OD1', wp, set())
+							self.max_diameter = max_diameter_cs.radius*2
+							self.advanced_faces = related_advanced_faces
+						else:
+							print 'circles_max_radius>max_radius'
 					else:
-						print 'circles_max_radius>max_radius'
+						print 'Doesnt contain 2 duplicated lines'
 				else:
-					print 'Doesnt contain 2 duplicated lines'
+					print 'Advances faces doesnt contain 4 lines'
+				'''
 			else:
-				print 'Advances faces doesnt contain 4 lines'
-			else:
-				print 'sd>max_diameter'''
+				print 'sd>max_diameter'
 		else:
 			print "Nooo"
